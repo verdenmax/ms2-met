2ed0aba2c02a3d96ea961713ab2a6a49dcfc92b5
kws <verden@88.com>
2025-11-22 17:32:53 +0800

    保存日志到文件

--- main.py
+++ main.py
@@ -15,14 +15,25 @@ def main():
     parser.add_argument(
         '--configpath', help='config.ini 文件路径，默认为 ./config.ini',
         default="./config.ini")
+    parser.add_argument(
+        '--logpath', help='日志文件路径，默认为 ./ms2.log',
+        default="./ms2.log")
     args = parser.parse_args()
 
     # 解析配置文件
     config = configparser.ConfigParser()
     config.read(args.configpath)
 
+    # 设置日志文件handle
+    file_handler = logging.FileHandler(args.logpath, encoding="utf-8")
+    file_formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+    file_handler.setFormatter(file_formatter)
+
     # 注册日志
-    logging.basicConfig(level=logging.INFO, handlers=[RichHandler()])
+    logging.basicConfig(level=logging.INFO, handlers=[
+                        RichHandler(), file_handler])
 
     # 展示开始的banner
     banner.show_start_banner()
